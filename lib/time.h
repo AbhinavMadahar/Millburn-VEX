600a8edd17afc4424245cfdf42c1d2fdf64dc442
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-18 20:21:48 -0400

    Added function to sleep() permanently

--- lib/time.h
+++ lib/time.h
@@ -17,3 +17,10 @@ Unless specified otherwise, all time units are in seconds
 inline void pause(const float unsigned duration sec) {
 	wait1Msec(1000 * duration); // convert to milliseconds
 }
+
+// marked as inline to avoid making a needlessly expensive function call
+inline void sleep() {
+	// it is more efficient to just wait for as long as possible between checks
+	while (true)
+		pause(60 sec);
+}
