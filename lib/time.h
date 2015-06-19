d3225db2a7bdb1499be0660c787fbae114204d23
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-18 20:21:22 -0400

    Added function to pause(...) for a given number of seconds

---
+++ lib/time.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+This file manages time-related activities such as:
+	* delaying
+
+Unless specified otherwise, all time units are in seconds
+*******************************************************************************/
+
+// force redefinition just in case included__time was marked as false
+#undef included__time
+#define included__time true
+
+#ifndef included__syntax
+	#include "syntax.h"
+#endif
+
+// marked as inline to avoid making a needlessly expensive function call
+inline void pause(const float unsigned duration sec) {
+	wait1Msec(1000 * duration); // convert to milliseconds
+}
