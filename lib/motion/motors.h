c6933516b9a00360f7d1608a6568e92113478190
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-21 12:57:26 -0400

    New file to store motor and sensor names

---
+++ lib/motion/motors.h
@@ -0,0 +1,7 @@
+/******************************************************************************
+This file names motors to make it easier to remember them
+******************************************************************************/
+
+// TODO: make sure these are the right ports
+#define motorLeft motorD
+#define motorRight motorE
