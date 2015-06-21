6751d2231e057264766aa0c17151473f22aa236e
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-21 14:52:21 -0400

    Added Color enum so we can use colors instead of numbers

---
+++ lib/sensation/color.h
@@ -0,0 +1,11 @@
+/******************************************************************************
+This file manages the color sensor and makes it simpler to use.
+
+Before using it, please make sure the correct #pragma is used
+TODO: figure out the right #pragma to use
+******************************************************************************/
+
+typedef enum Color {
+	black,
+	white
+};
