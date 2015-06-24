59272ae49509fc5969688113b263f330f03a346b
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-24 10:33:49 -0400

    Added file where we can put in the measurements
    These measurements are universal for almost every robot design, so
    I put them in. There are several for the field itself, which we will
    measure out.
    
    Later on, we will add more measurements as we make the robot more
    complex and add more in the way of tools and parts.

---
+++ lib/measurements.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+These are some of the robot's measurements, as well as measurements of the
+field.
+******************************************************************************/
+
+#ifndef included__syntax
+    #include "syntax.h"
+#endif
+
+// TODO: add the measurements for the robot's height
+const float robotHeight cm;
+const float robotLength cm;
+const float robotWidth cm;
+const float robotMass kg;
+
+// the field is a square, so the side lengths are all the same
+const float fieldLength = 365.76 cm; // 12 feet
+const float fieldHeight cm; // height of the walls
+
+const float goalHeight cm;
+const float goalSideLength cm;
+const float goalHypotenuse = sqrt(2 * goalSideLength * goalSideLength) cm;
+
+const float ballRadius cm;
+const float ballDiameter = 2 * ballRadius cm;
+const float ballVolume = 2 * PI * ballRadius * ballRadius cm;
