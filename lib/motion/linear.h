656c2991323b9c57d8a6567dc9db641f4b7ae1b2
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-30 16:50:49 -0400

    freeze() runs go(0)

--- lib/motion/linear.h
+++ lib/motion/linear.h
@@ -15,4 +15,7 @@ This file manages linear motion, like moving a certain speed forward.
 // when the robot design is decided, they will be implemented
 void go(const signed char cm per sec);
 void forward(const signed float cm);
-void freeze();
+
+void freeze() {
+	go(0);
+}
