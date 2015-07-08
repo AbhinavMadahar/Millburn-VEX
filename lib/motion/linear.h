682fd39e1a5b91ea23dac5641105c4bb3930f036
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-07-08 02:09:11 -0400

    Uses int to represent speed, not char
    This is because `char` is implied to be used to represent an ASCII
    character, so using it to represent a speed is not good practice.
    
    An int on the NXT Brick is 2 bytes, so just 1 byte more than char.
    That means that it is not a big deal as far as memory usage goes.

--- lib/motion/linear.h
+++ lib/motion/linear.h
@@ -9,7 +9,7 @@ This file manages linear motion, like moving a certain speed forward.
 
 // for now, these are just forward declarations
 // when the robot design is decided, they will be implemented
-void go(const signed char cm per sec);
+void go(const signed int cm per sec);
 void forward(const signed float cm);
 
 void freeze() {
