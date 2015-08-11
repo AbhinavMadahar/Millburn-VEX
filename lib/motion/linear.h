e667921230a329ff70e294e97ac8d6f904d1d265
Abhinav Madahar <AbhinavMadahar@users.noreply.github.com>
2015-08-11 14:09:30 -0400

    Partially implement go, goLeft, goRight
    I am not sure how you guys are going to set up the motor, so I
    just made some of the abstract logic. The parts that actually
    interact with the motors are not made yet.

--- lib/motion/linear.h
+++ lib/motion/linear.h
@@ -4,16 +4,41 @@ This file manages linear motion, like moving a certain speed forward.
 #ifndef included__linear
 #define included__linear true
 
-#include "motors.h"
 #include "../syntax.h"
+#include "motors.h"
 
-// for now, these are just forward declarations
-// when the robot design is decided, they will be implemented
-void go(int cm per sec);
-void forward(float cm);
+// moves the entire robot forwards or backwards
+go(int speed) {
+	speed = abs(speed) > 100 ? sgn(speed) * 100 : speed;
+	// TODO: make it move forwards/backwards
+}
 
-void freeze() {
+goLeft(int speed) {
+	speed = abs(speed) > 100 ? sgn(speed) * 100 : speed;
+	// TODO: make it move left
+}
+
+goRight(int speed) {
+	speed = abs(speed) > 100 ? sgn(speed) * 100 : speed;
+	// TODO: make it move right
+}
+
+go(int speed, unsigned int duration) {
+	go(speed);
+	wait1Msec(duration);
 	go(0);
+}
+
+goLeft(int speed, unsigned int duration) {
+	goLeft(speed);
+	wait1Msec(duration);
+	goLeft(0);
+}
+
+goRight(int speed, unsigned int duration) {
+	goRight(speed);
+	wait1Msec(duration);
+	goRight(0);
 }
 
 #endif
