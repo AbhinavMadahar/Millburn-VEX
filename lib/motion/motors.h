c03761bd88628bfe921f13db21d84602acabc577
Abhinav Madahar <AbhinavMadahar@users.noreply.github.com>
2015-08-11 13:46:29 -0400

    Implement moveMotor correctly and with comments
    moveMotor was made with quickly and the following problems got into it:
    * did not wait for the time to pass
    * did not stop after the duration had passed
    * unusual names like power
    * misleading names like speed, which was supposed to be duration
    * poor names like motorx
    * no checks to make sure speed was valid
    * no comments at all

--- lib/motion/motors.h
+++ lib/motion/motors.h
@@ -4,9 +4,18 @@ This file names motors to make it easier to remember them
 #ifndef included__motors
 #define included__motors true
 
-void moveMotor(TMotors motorx, int power，int speed) {
-	motor[motorx] = power;
+// moves a motor for a given amount of time then turns it off
+// motorToMove is the motor itself (like motorA), not just the port (like 2)
+// that is why motorToMove is declared as a TMotors, not an int
+// speed is a percentage from -100 to 100
+// duration is recorded in milliseconds
+void moveMotor(TMotors motorToMove, int speed，unsigned int duration) {
+	// make sure speed is in the range of [-100, 100]
+	speed = abs(speed) > 100 ? sgn(speed) * 100 : speed;
+	
+	motor[motorToMove] = speed;
+	wait1Msec(duration);
+	motor[motorToMove] = 0;
 }
-
 
 #endif
