9838e7f9f0382833a88cd6c11ad4ea7f6d79eb0d
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-11-05 16:00:13 -0500

    Add motor pragmas
    These are the ones we used before, and are already confirmed to work
    with the robot. However, the build team has a tendency to rewire things,
    so stay tentative.

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#pragma config(Motor,  port2,           launchBottomLeft,      tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           launchTopLeft,      tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port4,           launchTopRight,      tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port5,           launchBottomRight,      tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port6,           wheelBackLeft,       tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port7,           wheelFrontLeft,       tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port8,           wheelFrontRight,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port9,           wheelBackRight,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port10,          conveyor,      tmotorVex393_HBridge, openLoop, reversed)
+
 #include "autonomous.c"
 #include "manual.c"
 
