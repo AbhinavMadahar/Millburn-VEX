8ab092e1d3c16e839ed86423f8f087379f2ac92e
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-11-06 14:44:59 -0500

    isAlmost(0, 0) returns true
    Previously this would have made a big error and probably crashed the
    process

--- lib.c
+++ lib.c
@@ -9,9 +9,12 @@ bool isAlmost(int value, int constant) {
 		int aux = constant;
 		constant = value;
 		value = aux;
+	} else if (constant == 0 && value == 0) {
+		return true;
+	} else {
+		const float minimumAccuracy = 95.0;
+		return abs((value - constant) / constant) <= (1 - minimumAccuracy / 100);
 	}
-	const float minimumAccuracy = 95.0;
-	return abs((value - constant) / constant) <= (1 - minimumAccuracy / 100);
 }
 
 void setShooterSpeed(int speed) {
