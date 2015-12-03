68354d647820983f44f87e9ebc9488fd4cb1f2fd
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-12-03 16:58:08 -0500

    No longer using touch sensor

--- lib/sensation/touch.c
+++
@@ -1,9 +0,0 @@
-int isPressed(tSensors sensor = senseTouchFront) {
-	const int totalChecks = 5;
-	for (int i; i < totalChecks; i++) {
-		if (SensorValue[sensor] == 1) {
-			return true;
-		}
-	}
-	return false;
-}
